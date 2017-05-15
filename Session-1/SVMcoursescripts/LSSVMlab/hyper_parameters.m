6a63f503e9add00917d37040c1e61500e266da76
Mari <mari.a.muthu@gmail.com>
2017-05-15 22:32:12 +0200

    updated

--- Session-1/SVMcoursescripts/LSSVMlab/hyper_parameters.m
+++ Session-1/SVMcoursescripts/LSSVMlab/hyper_parameters.m
@@ -72,8 +72,28 @@ disp(performance_cv)
 disp(performance_so)
 
 
-%model = {X,Y,'c',[],[],'RBF_kernel','csa'};
-%[gam_1,sig2_1,cost_1] = tunelssvm(model,'simplex', 'crossvalidatelssvm',{10,'misclass'});
+model = {X,Y,'c',[],[],'RBF_kernel','csa'};
+[gam_1,sig2_1,cost_1] = tunelssvm(model,'simplex', 'crossvalidatelssvm',{10,'misclass'});
 
-model = {X,Y,'c',[],[],'RBF_kernel','ds'};
-[gam_2,sig2_2,cost_2] = tunelssvm(model,'simplex', 'crossvalidatelssvm',{10,'misclass'});
+%model = {X,Y,'c',[],[],'RBF_kernel','ds'};
+%[gam_2,sig2_2,cost_2] = tunelssvm(model,'simplex', 'crossvalidatelssvm',{10,'misclass'});
+
+model = {X,Y,'c',[],[],'RBF_kernel','csa'};
+[gam_3,sig2_3,cost_3] = tunelssvm(model,'gridsearch', 'crossvalidatelssvm',{10,'misclass'});
+
+%model = {X,Y,'c',[],[],'RBF_kernel','ds'};
+%[gam_4,sig2_4,cost_4] = tunelssvm(model,'gridsearch', 'crossvalidatelssvm',{10,'misclass'});
+
+y=[gam_1 sig2_1;gam_3 sig2_3];
+bar(y)
+Labels = {'csa & simplex', 'csa & gridsearch'};
+set(gca, 'XTick', 1:2, 'XTickLabel', Labels);
+legend('gam', 'sig2');
+
+%ROC
+
+[alpha,b] = trainlssvm({Xtrain,Ytrain,'c',gam,sig2, 'RBF_kernel'});
+[Ysim,Ylatent] = simlssvm({Xtrain,Ytrain,'c',gam,sig2, ...
+'RBF_kernel'},{alpha,b},Xval);
+[tpr,fpr,thresholds]=roc(Ysim,Yval);
+
